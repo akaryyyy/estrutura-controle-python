d1086c232a1ea3d585d6a39d7dde8840bfb47787
Diogo Souza <diogosouza.santus@gmail.com>
2026-02-03 22:33:42 -0300

    Atividade 03/02

---
+++ menuinterativo.py
@@ -0,0 +1,29 @@
+print("Menu de atendimento do Senac")
+
+print("Escolha uma das opções abaixo:")
+print("[1] Falar com atendente")
+print("[2] Segunda via de boleto")
+print("[3] Cancelar serviço")
+print("[4] Informações sobre planos")
+print("[5] Sair")
+
+option = input("Digite a opção desejada: ")
+
+match option:
+    case "1" | "Falar com atendente":
+        print("Você está em contato com o atendente")
+
+    case "2" | "Segunda via de boleto":
+        print("Você está na segunda via de boleto")
+
+    case "3" | "Cancelar serviço":
+        print("Você está no cancelamento de serviço")
+
+    case "4" | "Informações sobre planos":
+        print("Você está nos informativos de planos")
+
+    case "5" | "Sair":
+        print("Você saiu")
+
+    case _:
+        print("Opção inválida, tente novamente!")
